7cb55754afd3a82d1a650ae42867084885614179
Atis Dubrovskis <53487681+atisDu@users.noreply.github.com>
2023-09-12 11:14:30 +0300

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,13 @@
+import math;
+print("Sveika, Pasaule!");
+print("C ir pārāks"); #programmas nosaukums
+# teksts jaunā rindā
+i = math.sin(30);
+print("bāc\nbāc\nbāc","\nēdu sēnes ik dienu!\n");
+
+print("\tēdu \tsēnes, \tbaravikas");
+
+#atsevišķa print funkcija, bet visi vārdi vienā
+print("Veni", end="$")
+print("Vidi", end="$")
+print("Vici", end="$")
