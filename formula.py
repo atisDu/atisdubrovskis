2c98fcbcb951edf07f7a69712edb5e3978b25788
Atis Dubrovskis <53487681+atisDu@users.noreply.github.com>
2023-12-12 08:44:12 +0200

    BluH!

---
+++ formula.py
@@ -0,0 +1,9 @@
+startaNr = {1,2,3,4,5,6,7,8,9,10,11,12}
+
+S = int(input("Ievadi primo na"))
+
+pirmais = S(S <= 10**5 )
+otrais = N(N <= 10**5, S * N <= 10**6)
+
+
+punkti1 = dict.fromkeys(startaNr, rez1)
---
+++ latinu_kvadrats.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    int simbols;
+    scanf("%c", &simbols);
+    printf("%d", simbols);
+
+}
